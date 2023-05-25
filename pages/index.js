5c30ec585e4998c651fa3d0c2fdb404cf81d63bb
ayush <ayush.chauhan0008@gmail.com>
2023-05-25 14:32:08 +0530

    rendering from mongoDB successful

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,5 @@
+import { MongoClient } from "mongodb"
 import MeetupList from "../components/meetups/MeetupList"
-
-const fakeData = [
-    {
-        id: 'm1',
-        title: 'first meetup',
-        image: 'https://images.unsplash.com/photo-1580934738416-ad531f2920f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWV4aWNvJTIwY2l0eXxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
-        address: 'mexico city',
-        description: 'old barn in a village'
-    },
-    {
-        id: 'm2',
-        title: 'second meetup',
-        image: 'https://images.unsplash.com/photo-1682949029799-c1c111344b6f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=839&q=80',
-        address: 'jodhpur',
-        description: 'a peacock carving'
-    },
-
-]
 
 export default function Home(props) {
     return (
@@ -24,11 +7,27 @@ export default function Home(props) {
     )
 }
 
-
 // will not fetch new data added after building and deploying
 export async function getStaticProps() {
+    // fetch from mongoDB server
+    MongoClient.connect()
+
+    const client = await MongoClient.connect('mongodb+srv://ayush:new_mongoDB@cluster0.4vg9aiz.mongodb.net/meetups?retryWrites=true&w=majority')
+    const db = client.db()
+    const meetupsCollection = db.collection('meetups')
+
+    const meetupsFromMongo = await meetupsCollection.find().toArray()
+    client.close()
+
     return {
-        props: { meetups: fakeData },
+        props: {
+            meetups: meetupsFromMongo.map(meetup => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString(),
+            }))
+        },
         // to re-render or re-validate after following seconds
         revalidate: 10,
     }
