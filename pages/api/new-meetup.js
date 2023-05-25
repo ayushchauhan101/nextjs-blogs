ce974aeb561b2fe59e2c8af64b8f0776dbf18e9e
ayush <ayush.chauhan0008@gmail.com>
2023-05-25 14:06:34 +0530

    mongoDb connection function

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,23 @@
 //  domain/api/new-meetup
+import { MongoClient } from "mongodb"
 
 // only POST request trigger this route
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body
 
-        const { title, image, address, description } = data
+        // mongodb+srv://<username>:<password>@<cluster-name>.<region>.mongodb.net/<database-name>
+        const client = await MongoClient.connect('mongodb+srv://ayush:new_mongoDB@cluster0.4vg9aiz.mongodb.net/meetups?retryWrites=true&w=majority')
+        const db = client.db()
+
+        const meetupsCollection = db.collection('meetups')
+        const result = await meetupsCollection.insertOne(data)
+
+        console.log(result)
+
+        client.close()
+
+        res.staus(201).json({ message: 'Meetup inserted...' })
     }
 }
 
