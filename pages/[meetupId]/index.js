7fc4e8f6656e8737dd74130a9dce25394b330343
ayush <ayush.chauhan0008@gmail.com>
2023-05-25 12:45:49 +0530

    ssr

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -5,6 +5,7 @@ export default function Meetup() {
     const router = useRouter()
 
     const meetupId = router.query.meetupId
+    console.log('Currently at page: ',meetupId)
 
     return (
         <>
@@ -16,4 +17,34 @@ export default function Meetup() {
             />
         </>
     )
+}
+
+export async function getStaticPaths() {
+    return {
+        // if true nextJS tries to generate a new page itself
+        // if fallback true then params not defined will return a 404
+        fallback: false,
+        paths: [
+            { params: { meetupId: 'm1' } },
+            { params: { meetupId: 'm2' } },
+        ]
+    }
+}
+
+// this code runs during build time; runs in server side and not broswer in dev environment
+export async function getStaticProps(context) {
+    // to fetch data for a single meetup
+    const meetupId = context.params.meetupId
+
+    return {
+        props: {
+            meetupData: {
+                image: 'https://images.unsplash.com/photo-1580934738416-ad531f2920f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWV4aWNvJTIwY2l0eXxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+                id: meetupId,
+                title: 'Mexico city plaza',
+                address: 'Mexico city',
+                description: 'Busy plaza in mexico city',
+            }
+        }
+    }
 }
