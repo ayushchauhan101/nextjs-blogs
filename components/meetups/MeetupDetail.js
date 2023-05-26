3d1bac116bb4d04f5982ea860a11d89cad28ede0
ayush <ayush.chauhan0008@gmail.com>
2023-05-26 11:08:52 +0530

    add eslint and lint the app

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import classes from './MeetupDetail.module.css'
 
 const MeetupDetail = (props) => {
     return (
         <section className={classes.detail}>
-            <img
+            <Image
                 src={props.image}
                 alt={props.title} />
             <article>{props.address}</article>
